110e7490b0edc847c892216047c88343bd481c1c
Eric Kim <dgkim@dgkim.net>
2025-03-11 10:40:39 +0900

    fix: typing error
    npm run dev에서는, 이런 에러는 콘솔에 안 찍어주네..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Typography variant="h2">Deokgon Kim's nextjs site</Typography>
+        <Typography variant="h2">Deokgon Kim&apos;s nextjs site</Typography>
         <Image
           className="dark:invert"
           src="/next.svg"
